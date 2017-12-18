11b4f1a8bb4c9527a7df3ce2445bfe79e8de7607
Muhammad Bayu Devara <devarabayu@gmail.com>
2017-12-18 17:55:17 +0700

    Commit

---
+++ app/src/main/java/com/example/bayudevara/login/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.bayudevara.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Bayu Devara on 12/18/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
